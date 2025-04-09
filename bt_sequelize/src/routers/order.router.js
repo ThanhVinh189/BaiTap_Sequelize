b70439a75c1acf22501ea2d5373c8701345e899b
ThanhVinh189 <tham13734@gmail.com>
2025-04-09 14:50:44 +0700

    Update order.router.js

--- bt_sequelize/src/routers/order.router.js
+++ bt_sequelize/src/routers/order.router.js
@@ -3,13 +3,13 @@ import orderController from "../controllers/order.controller";
 
 const orderRouter = express.Router();
 
-// Endpoint tạo đơn hàng mới
+// Thêm đơn hàng mới
 orderRouter.post("/orders", orderController.addOrder);
 
-// Endpoint lấy danh sách đơn hàng (có thể lọc theo userId hoặc foodId)
+// Lấy danh sách đơn hàng (có thể lọc theo userId hoặc foodId)
 orderRouter.get("/orders", orderController.getOrders);
 
-// Endpoint xóa đơn hàng theo order_id
+// Xóa đơn hàng theo order_id
 orderRouter.delete("/orders/:order_id", orderController.deleteOrder);
 
 export default orderRouter;
